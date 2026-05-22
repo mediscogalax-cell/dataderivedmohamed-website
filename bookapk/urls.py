4f4cc44ab5e3cc21acd4e8cccf02a8466b7936d8
mohamed conteh <mediscogalax@gmail.com>
2026-05-22 17:11:14 -0400

    Add admin path to urlpatterns in urls.py

--- bookapk/urls.py
+++ bookapk/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 from portfolio import views
 
 urlpatterns = [
-       path('admin/', admin.site.urls),
         path('',include('portfolio.urls'),
+        path('admin/', admin.site.urls),
    
 ]
