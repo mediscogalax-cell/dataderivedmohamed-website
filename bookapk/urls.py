84ad9d3b07d0c890e3cb1971ef0aeb4d08995bd5
mohamed conteh <mediscogalax@gmail.com>
2026-05-22 16:16:40 -0400

    Fix import statement for views in urls.py

--- bookapk/urls.py
+++ bookapk/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from portfolio import views
+from . import views
 
 urlpatterns = [
     path('',views.about, name='about'),
